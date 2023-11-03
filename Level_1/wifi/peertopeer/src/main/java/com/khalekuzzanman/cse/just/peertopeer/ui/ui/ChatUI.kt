0f02177787a484df3a1ee032a58b9853741e93f0
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-11-03 12:14:09 +0600

    Wifi turn off/on control added on Top bar in NearByDeviceScreen.

---
+++ Level_1/wifi/peertopeer/src/main/java/com/khalekuzzanman/cse/just/peertopeer/ui/ui/ChatUI.kt
@@ -0,0 +1,8 @@
+package com.khalekuzzanman.cse.just.peertopeer.ui.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChatScreen() {
+
+}
--- Level_1/wifi/peertopeer/src/main/java/com/khalekuzzanman/cse/just/peertopeer/ui/ui/NearByDeviceScreen.kt
+++ Level_1/wifi/peertopeer/src/main/java/com/khalekuzzanman/cse/just/peertopeer/ui/ui/NearByDeviceScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Wifi
+import androidx.compose.material.icons.filled.WifiOff
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,16 +28,30 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.khalekuzzanman.cse.just.peertopeer.data_layer.WifiAndBroadcastHandler
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class NearByDeviceScreenModel {
+    private val _wifiEnabled = MutableStateFlow(false)
+    val wifiEnabled = _wifiEnabled.asStateFlow()
+    fun onWifiStatusChangeRequest() {
+        _wifiEnabled.update { !it }
+    }
+
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -47,6 +63,11 @@ fun NearByDeviceScreen() {
     val wifiManager = remember {
         WifiAndBroadcastHandler(context)
     }
+    val state = remember {
+        NearByDeviceScreenModel()
+    }
+    val wifiEnabled = state.wifiEnabled.collectAsState().value
+
     LaunchedEffect(Unit) {
         wifiManager.registerBroadcast()
         wifiManager.scanDevice()
@@ -63,6 +84,28 @@ fun NearByDeviceScreen() {
                     IconButton(onClick = { /*TODO*/ }) {
                         Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
                     }
+                },
+                actions = {
+                    IconButton(onClick = state::onWifiStatusChangeRequest) {
+                        if (wifiEnabled) {
+                            Icon(
+                                imageVector =
+                                Icons.Filled.WifiOff,
+                                contentDescription = null,
+                                tint = Color.Red
+                            )
+                        } else {
+                            Icon(
+                                imageVector =
+                                Icons.Filled.Wifi,
+                                contentDescription = null,
+                                tint = Color.Blue
+                            )
+
+                        }
+
+                    }
+
                 }
             )
         }
@@ -89,126 +132,4 @@ fun NearByDeviceScreen() {
 
 }
 
-@Preview
-@Composable
-fun DeviceListPreview() {
-    NearByDevices(
-        devices = listOf("Md Abul Kalam", "Mr Bean", "Galaxy Tab", "Sumsung A5"),
-        connectedDeviceIndex = 0
-    )
 
-}
-
-@Composable
-fun NearByDevices(
-    devices: List<String>,
-    connectedDeviceIndex: Int = -1,
-) {
-    var showDialog by remember {
-        mutableStateOf(false)
-    }
-    var connected by remember {
-        mutableStateOf(false)
-    }
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(8.dp),
-        shadowElevation = 1.dp
-
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        ) {
-            devices.forEachIndexed { index, name ->
-                val isConnected = connectedDeviceIndex == index
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    if (isConnected) {
-                        Icon(imageVector = Icons.Filled.Check, contentDescription = null)
-                    } else {
-                        Icon(imageVector = Icons.Filled.Wifi, contentDescription = null)
-                    }
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(text = name, style = MaterialTheme.typography.titleMedium)
-                        if (isConnected) {
-                            Text(text = "Connected", style = MaterialTheme.typography.labelSmall)
-                        }
-
-                    }
-                    IconButton(onClick = {
-                        showDialog = true
-                        connected = isConnected
-                    }) {
-                        Icon(imageVector = Icons.Outlined.Info, contentDescription = null)
-                    }
-                }
-
-            }
-
-        }
-        if (showDialog) {
-            ConnectedDeviceInfo(
-                onClose = { showDialog = false },
-                onDisconnectRequest = { showDialog = false },
-                isConnected = connected
-            )
-        }
-
-
-    }
-
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ConnectedDeviceInfo(
-    isConnected: Boolean,
-    onClose: () -> Unit,
-    onDisconnectRequest: () -> Unit
-) {
-
-    AlertDialog(
-        onDismissRequest = onClose,
-        modifier = Modifier
-    ) {
-        Surface(
-            modifier = Modifier,
-            shape = RoundedCornerShape(32.dp)
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-            ) {
-                Text(text = "Device Name: ")
-                Text(text = "IP Address: ")
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    Spacer(modifier = Modifier.weight(1f))
-                    if (isConnected) {
-                        TextButton(onClick = onDisconnectRequest) {
-                            Text(text = "Disconnect")
-                        }
-                    } else {
-                        TextButton(onClick = onClose) {
-                            Text(text = "Cancel")
-                        }
-                    }
-
-                }
-            }
-
-        }
-
-
-    }
-
-
-}
