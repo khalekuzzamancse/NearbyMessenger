c6ce980d4f8ef67f4e77b92faa7c7cd92f0bb15d
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-11-03 11:07:40 +0600

    NearByDevices Composable added,ScanDevice Composable changed to NearByDeviceScreen

---
+++ Level_1/wifi/peertopeer/src/main/java/com/khalekuzzanman/cse/just/peertopeer/ui/ui/NearByDeviceScreen.kt
@@ -0,0 +1,214 @@
+package com.khalekuzzanman.cse.just.peertopeer.ui.ui
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Wifi
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.khalekuzzanman.cse.just.peertopeer.data_layer.WifiAndBroadcastHandler
+
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@Preview
+@Composable
+fun NearByDeviceScreen() {
+    //
+    val context = LocalContext.current
+    val wifiManager = remember {
+        WifiAndBroadcastHandler(context)
+    }
+    LaunchedEffect(Unit) {
+        wifiManager.registerBroadcast()
+        wifiManager.scanDevice()
+
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Nearby Devices", style = MaterialTheme.typography.titleLarge)
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { scaffoldPadding ->
+        Column(
+            modifier = Modifier
+                .padding(scaffoldPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+            //    DeviceList(devices = wifiManager.scannedDevice.collectAsState().value)
+            NearByDevices(
+                devices = listOf("Md Abul Kalam", "Mr Bean", "Galaxy Tab", "Sumsung A5"),
+                connectedDeviceIndex = 0
+            )
+
+        }
+
+
+    }
+
+
+}
+
+@Preview
+@Composable
+fun DeviceListPreview() {
+    NearByDevices(
+        devices = listOf("Md Abul Kalam", "Mr Bean", "Galaxy Tab", "Sumsung A5"),
+        connectedDeviceIndex = 0
+    )
+
+}
+
+@Composable
+fun NearByDevices(
+    devices: List<String>,
+    connectedDeviceIndex: Int = -1,
+) {
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+    var connected by remember {
+        mutableStateOf(false)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        shadowElevation = 1.dp
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            devices.forEachIndexed { index, name ->
+                val isConnected = connectedDeviceIndex == index
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (isConnected) {
+                        Icon(imageVector = Icons.Filled.Check, contentDescription = null)
+                    } else {
+                        Icon(imageVector = Icons.Filled.Wifi, contentDescription = null)
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(text = name, style = MaterialTheme.typography.titleMedium)
+                        if (isConnected) {
+                            Text(text = "Connected", style = MaterialTheme.typography.labelSmall)
+                        }
+
+                    }
+                    IconButton(onClick = {
+                        showDialog = true
+                        connected = isConnected
+                    }) {
+                        Icon(imageVector = Icons.Outlined.Info, contentDescription = null)
+                    }
+                }
+
+            }
+
+        }
+        if (showDialog) {
+            ConnectedDeviceInfo(
+                onClose = { showDialog = false },
+                onDisconnectRequest = { showDialog = false },
+                isConnected = connected
+            )
+        }
+
+
+    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ConnectedDeviceInfo(
+    isConnected: Boolean,
+    onClose: () -> Unit,
+    onDisconnectRequest: () -> Unit
+) {
+
+    AlertDialog(
+        onDismissRequest = onClose,
+        modifier = Modifier
+    ) {
+        Surface(
+            modifier = Modifier,
+            shape = RoundedCornerShape(32.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(text = "Device Name: ")
+                Text(text = "IP Address: ")
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    if (isConnected) {
+                        TextButton(onClick = onDisconnectRequest) {
+                            Text(text = "Disconnect")
+                        }
+                    } else {
+                        TextButton(onClick = onClose) {
+                            Text(text = "Cancel")
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+
+    }
+
+
+}
