31847f834f5bc3aadd8f933c80cbda1854f11fc8
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-11-08 17:22:59 +0600

    unusable module removed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,10 +14,6 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "ConnectivitySamples(Networking)"
-include(":app")
 include(":Level_1")
-include(":Level_1:clinet_server_concept")
-include(":Level_1:clinet_server_concept:clinetapp")
-include(":Level_1:clinet_server_concept:serverapp")
 include(":Level_1:wifi")
 include(":Level_1:wifi:peertopeer")
