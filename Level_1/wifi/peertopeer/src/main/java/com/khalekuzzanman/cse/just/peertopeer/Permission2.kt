38cd22d882ed78c1bed2b201e8a5701267b48294
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-11-08 18:51:29 +0600

    Permission management added

--- Level_1/wifi/peertopeer/src/main/java/com/khalekuzzanman/cse/just/peertopeer/Permission2.kt
+++ Level_1/wifi/peertopeer/src/main/java/com/khalekuzzanman/cse/just/peertopeer/Permission2.kt
@@ -2,7 +2,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.accompanist.permissions.rememberPermissionState
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -16,6 +18,21 @@ fun PermissionManage(
     LaunchedEffect(Unit) {
         if (!allGranted) {
             state.launchMultiplePermissionRequest()
+        }
+    }
+
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun PermissionManage(
+    permission: String
+) {
+    val state = rememberPermissionState(permission)
+    val isNotGranted = !state.status.isGranted
+    LaunchedEffect(Unit) {
+        if (isNotGranted) {
+            state.launchPermissionRequest()
         }
     }
 
